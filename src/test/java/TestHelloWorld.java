a1ce80ee31b669a24e4d8acd70079f410dbb68c6
nsalin <stelian.alin@yahoo.com>
2019-10-24 17:13:48 +0300

    update to to trigger the webhook

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class TestHelloWorld {
     @Test
     public void testHelloWorld(){
-        System.out.println("Test Hello World");
+        System.out.println("Test Hello World - edited to trigger the jenkins webhook");
     }
 }
