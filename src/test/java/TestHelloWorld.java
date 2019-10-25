089acda2fc929d8f2ba935f88748544c6a64f01f
nsalin <stelian.alin@yahoo.com>
2019-10-25 17:25:54 +0300

    Update TestHelloWorld.java

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class TestHelloWorld {
     @Test
     public void testHelloWorld(){
-        System.out.println("Test Hello World - edited to trigger the jenkins webhook - NGROK Jenkins repo1 - test2");
+        System.out.println("Test Hello World - edited to trigger the jenkins webhook - NGROK Jenkins repo1 - test2 with multiple");
     }
 }
